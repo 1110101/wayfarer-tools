c441e4c66ffb34506c3e7e5bfef794dd96d98bf2
hedger <hedger@nanode.su>
2017-05-23 22:07:52 +0300

    Better Yandex maps location formatting (ty Itey), moved nearby portal strip on top of maps & made it larger

--- opr-tools.user.js
+++ opr-tools.user.js
@@ -145,7 +145,7 @@ width: 350px !important;
             mapButtons.push("<a class='button btn btn-default' target='_blank' href='https://www.ingress.com/intel?ll=" + data.lat + "," + data.lng +  "&z=17'>Intel</a>");
             mapButtons.push("<a class='button btn btn-default' target='_blank' href='https://bing.com/maps/default.aspx?cp=" + data.lat + "~" + data.lng +  "&lvl=16&style=a'>bing</a>");
             mapButtons.push("<a class='button btn btn-default' target='_blank' href='https://www.openstreetmap.org/?mlat=" + data.lat + "&mlon=" + data.lng +  "&zoom=16'>OSM</a>");
-            mapButtons.push("<a class='button btn btn-default' target='_blank' href='https://yandex.ru/maps/?ll=" + data.lng + "," + data.lat +  "&z=16'>Yandex</a>");
+            mapButtons.push("<a class='button btn btn-default' target='_blank' href='https://maps.yandex.ru/?text=" + data.lat + "," + data.lng +  "'>Yandex</a>");
             mapButtons.push("<a class='button btn btn-default' target='_blank' href='http://wikimapia.org/#lang=ru&lat=" + data.lat + "&lon=" + data.lng + "&z=16'>Wikimapia</a>");
 
             // more buttons in a dropdown menu
@@ -206,6 +206,10 @@ width: 350px !important;
             for (var i  = nodesToMove.length-1; i >= 0; --i) {
                 scorePanel.insertBefore(nodesToMove[i], scorePanel.firstChild);
             }
+
+            // Moving nearby portal strip higher
+            var mapsDiv = document.querySelector('form div.row[jstcache]');
+            mapsDiv.prepend(filmstrip);
 
             watchAdded = true;
 
