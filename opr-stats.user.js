636ed18e577f399ad71914daa6dc163fc6f4f43e
knut <knut.pfefferkorn@facebook.com>
2018-01-26 00:24:01 +0100

    dot Notation

--- opr-stats.user.js
+++ opr-stats.user.js
@@ -21,7 +21,7 @@
 	document.getElementById('oprt-stats').addEventListener('click', function(){
 		body.innerHTML = null;
         for (var i = 0; i < oprtstats.length;i++) {
-            body.insertAdjacentHTML("beforeEnd", YMDfromTime(oprtstats[i]['datum']) + ';' + oprtstats[i]['reviewed'] + ';' + oprtstats[i]['accepted'] + ';' + oprtstats[i]['rejected'] + '<br/>');
+            body.insertAdjacentHTML("beforeEnd", YMDfromTime(oprtstats[i].datum) + ';' + oprtstats[i].reviewed + ';' + oprtstats[i].accepted + ';' + oprtstats[i].rejected + '<br/>');
         }
 	});
 
@@ -62,8 +62,8 @@
 
     for (var i = 0; i < oprtstats.length;i++) {
        ymd = YMDfromTime(oprtstats[i].datum);
-       let prozent = 100*(oprtstats[i]['accepted']+oprtstats[i]['rejected'])/ oprtstats[i]['reviewed'];
-       section.insertAdjacentHTML("beforeEnd", ymd +':  ' + oprtstats[i]['reviewed'] + ' / ' + oprtstats[i]['accepted'] + ' / ' + oprtstats[i]['rejected'] + ' - ' + prozent.toFixed(2) + '% <br>');
+       let prozent = 100*(oprtstats[i].accepted + oprtstats[i].rejected)/ oprtstats[i].reviewed;
+       section.insertAdjacentHTML("beforeEnd", ymd +':  ' + oprtstats[i].reviewed + ' / ' + oprtstats[i].accepted + ' / ' + oprtstats[i].rejected + ' - ' + prozent.toFixed(2) + '% <br>');
     }
 
 
