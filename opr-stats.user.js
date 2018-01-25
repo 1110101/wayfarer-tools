e97f4d251acd3100decc5dd22d36e79d9d8a007a
knut <knut.pfefferkorn@facebook.com>
2018-01-26 00:24:18 +0100

    bump Version

--- opr-stats.user.js
+++ opr-stats.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OPR stats
-// @version      0.1.3
+// @version      0.1.4
 // @description  save OPR statistics in local browser storage
 // @author       https://gitlab.com/fotofreund0815
 // @match        https://opr.ingress.com/
